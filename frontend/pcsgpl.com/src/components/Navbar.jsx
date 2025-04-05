92483a24abe9a522b4d35e5d7607dadbc93de302
DivyanshSuwalka <suwalkadivyansh63@gmail.com>
2025-04-05 23:21:11 +0530

    basic frontend ready.

--- frontend/pcsgpl.com/src/components/Navbar.jsx
+++ frontend/pcsgpl.com/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-blue-500 text-white p-4 shadow-md">
+    <nav className="bg-blue-500 text-white p-4 px-20 shadow-md">
       <div className="container mx-auto flex justify-between">
         <div className="text-lg font-bold">pcsgpl.com</div>
-        <div className="flex gap-4">
+        <div className="flex gap-8">
           <Link to="/" className="hover:underline">
             Home
           </Link>
