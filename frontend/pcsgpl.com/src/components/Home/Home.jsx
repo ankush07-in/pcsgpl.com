c10ed04a5dceb82fcae07b73e88c93ce04f62fe6
moose@97 <ankushkol35@gmail.com>
2025-04-13 11:44:53 +0530

    Text Home
    Added details in Home page

--- frontend/pcsgpl.com/src/components/Home/Home.jsx
+++ frontend/pcsgpl.com/src/components/Home/Home.jsx
@@ -5,10 +5,16 @@ function Home() {
       <div className="text-center">
         <Slider />
         <h1 className="text-3xl font-bold text-blue-600">
-          Welcome to Home Page
+          Welcome to PCS GLOBAL
         </h1>
         <p className="mt-4 text-gray-600">
-          This is the homepage of our website.
+          Perennation Computer Solutions Global Private Limited
+        </p>
+        <p className="mt-4 text-black-300">
+          PCS Global is a Web development company located in Bengaluru and Kolkata, India.
+        </p>
+        <p className="mt-2 text-yellow-900 text-sm">
+          We provide businesses with a centralized system to streamline and automate various processes related to customer interactions, sales pipeline management, analytics, and collaboration.
         </p>
       </div>
     </>
