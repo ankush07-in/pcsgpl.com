d3d1bf64f6a6a9171359daf1377dddec178f3b5b
moose@97 <ankushkol35@gmail.com>
2025-04-13 17:19:23 +0530

    Services
    Services Page added

--- frontend/pcsgpl.com/src/App.jsx
+++ frontend/pcsgpl.com/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
+import Services from "./components/Services/Services";
 
 // Main App Component
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </Router>
   );
